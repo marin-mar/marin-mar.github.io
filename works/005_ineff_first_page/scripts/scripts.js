25e5dd7a4ffa6ce37d4fe0c496539ce4516474b0
marin-mar <72693639+marin-mar@users.noreply.github.com>
2021-07-23 10:53:22 +0300

    refresh files

--- works/005_ineff_first_page/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let menuBurger = document.getElementById('menu__burger');
-let menuList = document.getElementById('menu__list');
-
-menuBurger.addEventListener('click', () => {
-    menuList.classList.toggle('menu__list--active');
-});
