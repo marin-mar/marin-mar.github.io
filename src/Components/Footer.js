1702f723856d9ae4b45b7dbaff5de0c272f9a48a
marin-mar <72693639+marin-mar@users.noreply.github.com>
2021-08-14 14:29:28 +0300

    change data

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     <Fragment>
       <footer className="container-fluid bg-gray fixed-bottom bg-light bg-gradient bg-opacity-90 d-flex flex-wrap flex-col flex-md-row justify-content-center">
         <p className="text-dark text-center pt-3 pb-1">
-          &copy; 2021 marinmar &copy;
+          &copy; marinmar &copy;
         </p>
       </footer>
     </Fragment>
